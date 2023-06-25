62c51c46149f3dcec27bcb954371aca2328ffac6
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-06-25 19:19:46 +0700

    chore: restart the setup in the src folder

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const Greeting = () => {
+  return <h1>Hi, Mom!</h1>
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const container = document.getElementById('id');
+const root = ReactDOM.createRoot(container);
+
+root.render(<Greeting/>);
+
+export default Greeting;
