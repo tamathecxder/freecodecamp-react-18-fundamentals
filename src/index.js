8bf64ebfe45e340a9ce277d26f812c067b63ef34
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-06-28 20:08:54 +0700

    chore: mapping books to component

--- src/index.js
+++ src/index.js
@@ -15,43 +15,34 @@ const Book = ({ title, author, image, children }) => {
 };
 
 const BookList = () => {
-  const firstBook = {
-    title: "Atomic Habits",
-    author: "James Clear",
-    image: "https://images-na.ssl-images-amazon.com/images/I/81bGKUa1e0L._AC_UL600_SR600,400_.jpg"
-  };
+  const books = [
+    {
+      title: "Atomic Habits",
+      author: "James Clear",
+      image:
+        "https://images-na.ssl-images-amazon.com/images/I/81bGKUa1e0L._AC_UL600_SR600,400_.jpg",
+    },
+    {
+      title: "Iron Flame",
+      author: "Rebecca Yaros",
+      image:
+        "https://images-na.ssl-images-amazon.com/images/I/A1bc9KkHtsL._AC_UL600_SR600,400_.jpg",
+    },
+    {
+      title: "The Covenant Of Water",
+      author: "Abraham Verghese",
+      image:
+        "https://images-na.ssl-images-amazon.com/images/I/91b7tm523VL._AC_UL600_SR600,400_.jpg",
+    },
+  ];
 
-  const secondBook = {
-    title: "Iron Flame",
-    author: "Rebecca Yaros",
-    image: "https://images-na.ssl-images-amazon.com/images/I/A1bc9KkHtsL._AC_UL600_SR600,400_.jpg"
-  };
-
-  const thirdBook = {
-    title: "The Covenant Of Water",
-    author: "Abraham Verghese",
-    image: "https://images-na.ssl-images-amazon.com/images/I/91b7tm523VL._AC_UL600_SR600,400_.jpg"
-  };
-  
   return (
     <section className="booklist">
-      <Book
-        title={firstBook.title}
-        author={firstBook.author}
-        image={firstBook.image}
-      >
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero nobis molestias debitis repellat, dolor hic.</p>
-      </Book>
-      <Book
-        title={secondBook.title}
-        author={secondBook.author}
-        image={secondBook.image}
-      />
-      <Book
-        title={thirdBook.title}
-        author={thirdBook.author}
-        image={thirdBook.image}
-      />
+      {books.map((value) => {
+        return (
+          <Book title={value.title} author={value.author} image={value.image} />
+        );
+      })}
     </section>
   );
 };
