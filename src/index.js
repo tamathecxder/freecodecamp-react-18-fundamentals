a0148fb066d80c7e3650eee6d8804c97229708ee
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-06-28 14:34:01 +0700

    refactor: merge book component

--- src/index.js
+++ src/index.js
@@ -3,35 +3,15 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 
-const Image = () => {
-  return (
-    <img
-      src="https://images-na.ssl-images-amazon.com/images/I/81bGKUa1e0L._AC_UL600_SR600,400_.jpg"
-      alt="Atomic Habits"
-    />
-  );
-};
-
-const Title = () => {
-  return <h2>Atomic Habits</h2>;
-};
-
-const Author = () => {
-  const inlineHeadingStyles = {
-    color: "#617d98",
-    fontSize: "0.75rem",
-    marginTop: "0.5rem",
-  };
-
-  return <h4 style={inlineHeadingStyles}>James Clear</h4>;
-};
-
 const Book = () => {
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/I/81bGKUa1e0L._AC_UL600_SR600,400_.jpg"
+        alt="Atomic Habits"
+      />
+      <h2>Atomic Habits</h2>
+      <h4>James Clear</h4>
     </article>
   );
 };
