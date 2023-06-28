473656c183a4420681312ee575d7e28b9eafa042
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-06-28 17:21:20 +0700

    chore: accessing props through object and pass it to the component

--- src/index.js
+++ src/index.js
@@ -3,25 +3,56 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 
-const Book = () => {
+const Book = (props) => {
+  const title = props.title;
+  const author = props.author;
+  const image = props.image;
+
   return (
     <article className="book">
-      <img
-        src="https://images-na.ssl-images-amazon.com/images/I/81bGKUa1e0L._AC_UL600_SR600,400_.jpg"
-        alt="Atomic Habits"
-      />
-      <h2>Atomic Habits</h2>
-      <h4>James Clear</h4>
+      <img src={image} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author.toUpperCase()}</h4>
     </article>
   );
 };
 
 const BookList = () => {
+  const firstBook = {
+    title: "Atomic Habits",
+    author: "James Clear",
+    image: "https://images-na.ssl-images-amazon.com/images/I/81bGKUa1e0L._AC_UL600_SR600,400_.jpg"
+  };
+
+  const secondBook = {
+    title: "Iron Flame",
+    author: "Rebecca Yaros",
+    image: "https://images-na.ssl-images-amazon.com/images/I/A1bc9KkHtsL._AC_UL600_SR600,400_.jpg"
+  };
+
+  const thirdBook = {
+    title: "The Covenant Of Water",
+    author: "Abraham Verghese",
+    image: "https://images-na.ssl-images-amazon.com/images/I/91b7tm523VL._AC_UL600_SR600,400_.jpg"
+  };
+  
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        title={firstBook.title}
+        author={firstBook.author}
+        image={firstBook.image}
+      />
+      <Book
+        title={secondBook.title}
+        author={secondBook.author}
+        image={secondBook.image}
+      />
+      <Book
+        title={thirdBook.title}
+        author={thirdBook.author}
+        image={thirdBook.image}
+      />
     </section>
   );
 };
@@ -34,8 +65,7 @@ const App = () => {
   );
 };
 
-const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(<App />);
 
