816422c63372686096527a19e2a026048df8b58f
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-06-29 13:44:04 +0700

    chore: separated the books array into separate folders

---
+++ src/data/books.js
@@ -0,0 +1,21 @@
+import img1 from '../images/img1.jpg';
+import img2 from '../images/img2.jpg';
+import img3 from '../images/img3.jpg';
+
+export const books = [
+  {
+    title: "Atomic Habits",
+    author: "James Clear",
+    image: img1,
+  },
+  {
+    title: "Iron Flame",
+    author: "Rebecca Yaros",
+    image: img2,
+  },
+  {
+    title: "The Covenant Of Water",
+    author: "Abraham Verghese",
+    image: img3,
+  },
+];
