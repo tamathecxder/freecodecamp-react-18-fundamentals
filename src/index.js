a1906f150548a9ac33a673accbfcbfee7d4443bd
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-06-29 13:15:20 +0700

    chore: invoking function in a component event

--- src/index.js
+++ src/index.js
@@ -4,65 +4,71 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 const Book = (props) => {
-  const { title, author, image, getParaghraph, children } = props;
+  const { title, author, image, getBook, id } = props;
+
+  // const handleBook = () => {
+  //   getBook(id);
+  // };
 
   return (
     <article className="book">
       <img src={image} alt={title} />
       <h2>{title}</h2>
-      <button onClick={getParaghraph}>Show Title</button>
+      {/* <button onClick={handleBook}>Show Title</button> */}
+      <button onClick={() => getBook(id)}>Show Title</button>
       <h4>{author.toUpperCase()}</h4>
-      {children}
     </article>
   );
 };
 
+const generateRandomString = (length) => {
+  let result = "";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
+
+const books = [
+  {
+    title: "Atomic Habits",
+    author: "James Clear",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/81bGKUa1e0L._AC_UL600_SR600,400_.jpg",
+  },
+  {
+    title: "Iron Flame",
+    author: "Rebecca Yaros",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/A1bc9KkHtsL._AC_UL600_SR600,400_.jpg",
+  },
+  {
+    title: "The Covenant Of Water",
+    author: "Abraham Verghese",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/91b7tm523VL._AC_UL600_SR600,400_.jpg",
+  },
+];
+
+const newBooks = books.map((book, index) => ({
+  ...book,
+  id: generateRandomString(9) + (index + 1),
+}));
+
+
 const BookList = () => {
-  const paragraph = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus, adipisci.";
-
-  const getParaghraph = () => console.log(paragraph);
-  
-  const books = [
-    {
-      title: "Atomic Habits",
-      author: "James Clear",
-      image:
-        "https://images-na.ssl-images-amazon.com/images/I/81bGKUa1e0L._AC_UL600_SR600,400_.jpg",
-    },
-    {
-      title: "Iron Flame",
-      author: "Rebecca Yaros",
-      image:
-        "https://images-na.ssl-images-amazon.com/images/I/A1bc9KkHtsL._AC_UL600_SR600,400_.jpg",
-    },
-    {
-      title: "The Covenant Of Water",
-      author: "Abraham Verghese",
-      image:
-        "https://images-na.ssl-images-amazon.com/images/I/91b7tm523VL._AC_UL600_SR600,400_.jpg",
-    },
-  ];
-
-  const generateRandomString = (length) => {
-    let result = "";
-    const characters =
-      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    const charactersLength = characters.length;
-    for (let i = 0; i < length; i++) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
+  const getBook = (id) => {
+    const book = newBooks.find((book) => book.id === id);
+    console.log(book);
   };
-
-  const newBooks = books.map((book, index) => ({
-    ...book,
-    id: generateRandomString(9) + (index + 1),
-  }));
 
   return (
     <section className="booklist">
       {newBooks.map((book) => {
-        return <Book {...book} getParaghraph={getParaghraph} key={book.id} />;
+        return <Book {...book} getBook={getBook} key={book.id} />;
       })}
     </section>
   );
