43d9f890ce18455a80ab1540b152384eafa99de8
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-06-29 11:49:15 +0700

    chore: avoid to use props drilling

--- src/index.js
+++ src/index.js
@@ -4,60 +4,24 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 const Book = (props) => {
-  const { title, author, image, children } = props;
+  const { title, author, image, getParaghraph, children } = props;
+
   return (
     <article className="book">
       <img src={image} alt={title} />
       <h2>{title}</h2>
+      <button onClick={getParaghraph}>Show Title</button>
       <h4>{author.toUpperCase()}</h4>
       {children}
     </article>
   );
 };
 
-const EventExamples = () => {
-  const handleFormInput = (event) => {
-    // 
-  };
-
-  const handleButtonClick = () => {
-    alert("Hi, Mom!");
-  };
-
-  const handleFormSubmit = (event) => {
-    event.preventDefault();
-
-    console.log("Form is submitted");
-  }
-
-  return (
-    <section>
-      <form onSubmit={handleFormSubmit}>
-        <h2>Typical Input</h2>
-        <input
-          type="text"
-          name="example"
-          id="example"
-          style={{ margin: "1rem 0" }}
-          onChange={handleFormInput}
-        />
-        <button type="submit">
-          Submit
-        </button>
-        <div style={{ display: "flex", gap: "10px" }}>
-          <button type="button" onClick={handleButtonClick}>
-            Click 1
-          </button>
-          <button type="button" onClick={() => console.log("Arrow function callback")}>
-            Click 2
-          </button>
-        </div>
-      </form>
-    </section>
-  );
-};
-
 const BookList = () => {
+  const paragraph = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus, adipisci.";
+
+  const getParaghraph = () => console.log(paragraph);
+  
   const books = [
     {
       title: "Atomic Habits",
@@ -97,9 +61,8 @@ const BookList = () => {
 
   return (
     <section className="booklist">
-      <EventExamples />
       {newBooks.map((book) => {
-        return <Book {...book} key={book.id} />;
+        return <Book {...book} getParaghraph={getParaghraph} key={book.id} />;
       })}
     </section>
   );
